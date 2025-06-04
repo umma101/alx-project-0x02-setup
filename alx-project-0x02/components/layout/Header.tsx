4e6485754a15ac8e70c88fa0de1c51a126d2e1a5
Mary <maryauma2606@gmail.com>
2025-06-04 18:12:29 +0300

    add post

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -4,11 +4,19 @@ const Header = () => {
   return (
     <header style={{ backgroundColor: '#000', color: '#fff', padding: '1rem' }}>
       <nav>
-        <Link href="/home" style={{ color: '#fff', marginRight: '1rem' }}>Home</Link>
-        <Link href="/about" style={{ color: '#fff' }}>About</Link>
+        <Link href="/home" legacyBehavior>
+          <a style={{ color: '#fff', marginRight: '1rem' }}>Home</a>
+        </Link>
+        <Link href="/about" legacyBehavior>
+          <a style={{ color: '#fff', marginRight: '1rem' }}>About</a>
+        </Link>
+        <Link href="/posts" legacyBehavior>
+          <a style={{ color: '#fff' }}>Posts</a>
+        </Link>
       </nav>
     </header>
   );
 };
 
 export default Header;
+
---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,20 @@
+// pages/posts.tsx
+import Header from '../components/layout/Header';
+
+const PostsPage = () => {
+  return (
+    <>
+      <Header />
+      <main className="p-4">
+        <h1 className="text-2xl font-bold">Our Posts</h1>
+      </main>
+    </>
+  );
+};
+
+export default PostsPage;
+
+
+
+
+
