6ba54abda38a604281cfed640667dcdaaa274dbd
Mary <maryauma2606@gmail.com>
2025-06-04 16:13:59 +0300

    add cards

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { CardProps } from '@/interfaces';
+
+const Card: React.FC<CardProps> = ({ title, content }) => {
+  return (
+    <div style={{
+      border: '1px solid #000',
+      borderRadius: '8px',
+      padding: '1rem',
+      marginBottom: '1rem',
+      backgroundColor: '#f9f9f9',
+      boxShadow: '0 2px 4px rgba(210, 23, 23, 0.1)',
+    }}>
+      <h2 style={{ fontSize: '1.5rem', marginBottom: '8px' }}>{title}</h2>
+      <p style={{ fontSize: '1rem', color: '#000' }}>{content}</p>
+    </div>
+  );
+};
+
+export default Card;
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,15 +1,31 @@
-import Header from '../components/layout/Header';
+import React from 'react';
+import Card from '@/components/common/Card';
+import Header from '@/components/layout/Header';
 
-const HomePage = () => {
+const Home = () => {
   return (
-    <>
+    <div>
       <Header />
       <main style={{ padding: '2rem' }}>
         <h1>Home Page</h1>
-        <p>This is the home page content.</p>
+
+<div style={{ display: 'flex', gap: '1rem' }}>
+          <Card title="Card One" content="This is the content of the first card." />
+          <Card title="Card Two" content="This is the content of the second card." />
+          <Card title="Card Three" content="Another piece of information goes here." />
+          <Card title="Card Four" content="This is the content of the first card." />
+          
+
+        </div>
+         <div style={{ display: 'flex', gap: '1rem' }}>
+          <Card title="Card One" content="This is the content of the first card." />
+          <Card title="Card Two" content="This is the content of the second card." />
+          <Card title="Card Three" content="Another piece of information goes here." />
+          <Card title="Card Four" content="This is the content of the first card." />
+        </div>
       </main>
-    </>
+    </div>
   );
 };
 
-export default HomePage;
+export default Home;
